f1e950a6ef181388cb76c7939a77b716938408ac
csmoke66 <johnd68172@gmail.com>
2026-07-13 11:28:10 -0400

    couple of small important notes

--- mod/src/main.cpp
+++ mod/src/main.cpp
@@ -6,6 +6,8 @@ void init_async()
     RS.init();
 }
 
+// TODO FIXME replace this with a global object to support
+// other compilers
 __attribute__((constructor)) void entry()
 {
     std::thread thr(init_async);
