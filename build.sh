de33a3dcc705c62aedecda810ba96e7520b791ea
csmoke66 <johnd68172@gmail.com>
2026-09-06 13:53:15 -0400

    improve build script more, make it generate reversed_generated.h

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
+INSTALL_LOCATION="$HOME/.local/share/cachy-rs"
+PLUGIN_LOCATION="$INSTALL_LOCATION/plugins"
+BOLT_LOCATION="$HOME/.local/share/bolt-launcher/Jagex/launcher"
 
 if [ ! -d "build" ]; then
     mkdir build
@@ -13,22 +16,29 @@ make
 
 cd ..
 
-if [ ! -d "$HOME/.local/share/cachy-rs" ]; then
-    mkdir -p ~/.local/share/cachy-rs
+if [ ! -d "$INSTALL_LOCATION" ]; then
+    mkdir -p "$INSTALL_LOCATION"
 fi
 
-if [ ! -d "$HOME/.local/share/cachy-rs/plugins" ]; then
-    mkdir ~/.local/share/cachy-rs/plugins
+if [ ! -d "$PLUGIN_LOCATION" ]; then
+    mkdir "$PLUGIN_LOCATION"
 fi
 
 echo "==> Updating file at $HOME/.local/share/cachy-rs..."
 cp -r config/* ~/.local/share/cachy-rs
-cp build/mod/libmod.so ~/.local/share/cachy-rs/libmod.so
-cp build/updater/updater ~/.local/share/cachy-rs/updater
+cp build/mod/libmod.so "$INSTALL_LOCATION/libmod.so"
+cp build/updater/updater "$INSTALL_LOCATION/updater"
 
-if [ -d "$HOME/.local/share/bolt-launcher/Jagex/launcher/" ]; then
-    echo "==> Moving rs2client from bolt launcher to $HOME/.local/share/cachy-rs..."
-    cp ~/.local/share/bolt-launcher/Jagex/launcher/rs2client ~/.local/share/cachy-rs/rs2client
+if [ -f "$BOLT_LOCATION/rs2client" ]; then
+    echo "==> Copying rs2client from $BOLT_LOCATION to $INSTALL_LOCATION"
+    cp "$BOLT_LOCATION/rs2client" "$INSTALL_LOCATION/rs2client"
+fi
+
+if [ -f "$INSTALL_LOCATION/rs2client" ]; then
+    echo "==> Generating reversed_generated.h"
+    cd "$INSTALL_LOCATION"
+    ./updater
+    echo "==> If your reversed_generated.h is outdated, a new one can be found at $INSTALL_LOCATION/reversed_generated.h"
 fi
 
 echo "==> Done!"
